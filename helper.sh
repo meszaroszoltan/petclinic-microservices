25c275822d4b7124f9518cf7b12a52fd8bbca3a3
Zoltán Mészáros <zoltan.meszaros@adnovum.hu>
2022-10-31 19:18:57 +0100

    Cleanup.

---
+++ helper.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+build_api() {
+  cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/spring-petclinic-api-gateway
+  ./mvnw spring-boot:build-image -DskipTests
+  minikube image load spring-petclinic-cloud-api-gateway
+  cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/
+}
+
+build_visits() {
+  cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/spring-petclinic-visits-service
+  ./mvnw spring-boot:build-image -DskipTests
+  minikube image load spring-petclinic-cloud-visits-service
+  cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/
+}
+
+build_vets() {
+  cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/spring-petclinic-vets-service
+  ./mvnw spring-boot:build-image -DskipTests
+  minikube image load spring-petclinic-cloud-vets-service
+  cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/
+}
+
+build_customers() {
+  cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/spring-petclinic-customers-service
+  ./mvnw spring-boot:build-image -DskipTests
+  minikube image load spring-petclinic-cloud-customers-service
+  cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/
+}
+
+build() {
+  build_api
+  build_visits
+  build_vets
+  build_customers
+}
+
+start() {
+  cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/
+  kubectl replace -f k8s --force
+  kubectl replace -f k8s/spring/vets-service.yaml --force
+  kubectl replace -f k8s/spring/visits-service.yaml --force
+  kubectl replace -f k8s/spring/customers-service.yaml --force
+  kubectl get all
+}
+
+expose() {
+    minikube service api-gateway --url
+}
+
+$1
+$2
+$3
