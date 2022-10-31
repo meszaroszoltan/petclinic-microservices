16b089378915515503856f66bbb1ebec670840f8
Zoltán Mészáros <zoltan.meszaros@adnovum.hu>
2022-10-31 16:57:01 +0100

    Removed Spring Cloud services.

--- scripts/pushImages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker push spring-petclinic-cloud-api-gateway:latest
-docker push spring-petclinic-cloud-visits-service:latest
-docker push spring-petclinic-cloud-vets-service:latest
-docker push spring-petclinic-cloud-customers-service:latest
-docker push spring-petclinic-cloud-admin-server:latest
-docker push spring-petclinic-cloud-discovery-service:latest
-docker push spring-petclinic-cloud-config-server:latest
-
