76cdb8036dd9224a3f6bdc6f3eb70adfbaec0539
Zoltán Mészáros <zoltan.meszaros@adnovum.hu>
2022-10-31 16:55:24 +0100

    Removed registry settings, since I will build and run the project locally.

--- scripts/pushImages.sh
+++ scripts/pushImages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-api-gateway:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-visits-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-vets-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-customers-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-admin-server:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-discovery-service:latest
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-cloud-config-server:latest
+docker push spring-petclinic-cloud-api-gateway:latest
+docker push spring-petclinic-cloud-visits-service:latest
+docker push spring-petclinic-cloud-vets-service:latest
+docker push spring-petclinic-cloud-customers-service:latest
+docker push spring-petclinic-cloud-admin-server:latest
+docker push spring-petclinic-cloud-discovery-service:latest
+docker push spring-petclinic-cloud-config-server:latest
 
