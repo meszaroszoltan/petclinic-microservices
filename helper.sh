3f0674e66b6b67fc47d5913b405fd97078d2ce87
Zoltán Mészáros <zoltan.meszaros@adnovum.hu>
2022-11-03 16:49:53 +0100

    Spring specific changes.

--- helper.sh
+++ helper.sh
@@ -1,41 +1,41 @@
 #!/usr/bin/env bash
 
-build_api() {
+spring_build_api() {
   cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/spring-petclinic-api-gateway
   ./mvnw spring-boot:build-image -DskipTests
   minikube image load spring-petclinic-cloud-api-gateway
   cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/
 }
 
-build_visits() {
+spring_build_visits() {
   cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/spring-petclinic-visits-service
   ./mvnw spring-boot:build-image -DskipTests
   minikube image load spring-petclinic-cloud-visits-service
   cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/
 }
 
-build_vets() {
+spring_build_vets() {
   cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/spring-petclinic-vets-service
   ./mvnw spring-boot:build-image -DskipTests
   minikube image load spring-petclinic-cloud-vets-service
   cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/
 }
 
-build_customers() {
+spring_build_customers() {
   cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/spring-petclinic-customers-service
   ./mvnw spring-boot:build-image -DskipTests
   minikube image load spring-petclinic-cloud-customers-service
   cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/
 }
 
-build() {
-  build_api
-  build_visits
-  build_vets
-  build_customers
+spring_build() {
+  spring_build_api
+  spring_build_visits
+  spring_build_vets
+  spring_build_customers
 }
 
-start() {
+spring_deploy() {
   cd /mnt/c/Users/zolim/IdeaProjects/spring-petclinic-cloud/
   kubectl replace -f k8s --force
   kubectl replace -f k8s/spring/vets-service.yaml --force
